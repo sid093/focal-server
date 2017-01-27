1b49a02a9a20f1bcf7d1414ea4ee085cb57c0e8a
Siddharth Srivastava <sid.pyd@gmail.com>
2017-01-27 23:36:35 +0530

    Updated Error page

--- focal.js
+++ focal.js
@@ -1,14 +1,18 @@
 var express = require('express');
-var userRoute = require('./routes/user')
-var adminRoute = require('./routes/admin')
-var mailboxRoute = require('./routes/mailbox')
-var locationRoute = require('./routes/location')
+var userRoute = require('./routes/user');
+var adminRoute = require('./routes/admin');
+var mailboxRoute = require('./routes/mailbox');
+var locationRoute = require('./routes/location');
+var authenticate = require('./functions/authenticate');
 var app = express();
 
-app.get('/', function (req, res) {
+app.use('/web', express.static(__dirname + '/web'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.all('/', function (req, res) {
   res.send('Index Page');
 });
 
+app.use(authenticate)
 app.use('/user', userRoute);
 app.use('/admin', adminRoute);
 app.use('/mailbox', mailboxRoute);
